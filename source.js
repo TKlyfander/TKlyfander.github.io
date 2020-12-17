c3fc281ede56389eaa71f1ea1995547f3757414b
TKlyfander <76142772+TKlyfander@users.noreply.github.com>
2020-12-17 16:32:42 -0300

    Delete source.js

--- source.js
+++
@@ -1,8 +0,0 @@
-function oplayer(){
-    document.getElementById("chose").style.display = "none";
-    document.getElementById("run").style.display = "block"; 
-	}
-function ocreator(){
-    document.getElementById("chose").style.display = "none";
-    document.getElementById("creator").style.display = "block"; 
-	}
