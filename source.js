aaff62ba45412417884ed5624e09114b32e2c27c
TKlyfander <76142772+TKlyfander@users.noreply.github.com>
2020-12-17 15:59:07 -0300

    Update source.js

--- source.js
+++ source.js
@@ -1,8 +1,8 @@
 function oplayer(){
-	document.getElementById("chose").style.display = "none";
-   document.getElementById("run").style.display = "block"; 
+    document.getElementById("chose").style.display = "none";
+    document.getElementById("run").style.display = "block"; 
 	}
 function ocreator(){
-	document.getElementById("chose").style.display = "none";
-   document.getElementById("creator").style.display = "block"; 
+    document.getElementById("chose").style.display = "none";
+    document.getElementById("creator").style.display = "block"; 
 	}
